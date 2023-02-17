228035642dcd19e77ad1f8b7f66980ca6c843882
cooper <mccoy.cooperf@gmail.com>
2023-02-17 09:23:13 -0600

    Finish section 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
 end
